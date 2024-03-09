8c5c9ab9f88db998799605eea623e3c23b3664b6
thomaslnx <marcos.msilva10@gmail.com>
2024-03-09 18:21:49 -0300

    Initial config and get route

---
+++ backend/src/server.ts
@@ -0,0 +1,25 @@
+import express, { Express, Request, Response } from 'express'
+const cors = require('cors')
+
+const server: Express = express()
+server.use(cors())
+server.use(express.json())
+
+let exampleIssues = [
+  {
+    id: 1,
+    title: "Example Issue 1",
+    description: "Example description Issue 1",
+  },
+  {
+    id: 2,
+    title: "Example Issue 2",
+    description: "Example description Issue 2",
+  },
+]
+
+server.get('/issues', (req: Request, res: Response) => {
+  return res.json(exampleIssues)
+})
+
+server.listen(3000, () => console.log('Server up and running!'))
